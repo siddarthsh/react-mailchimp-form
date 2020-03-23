eb18d215aa1e9d3e91dcfd9ac5c8fe32dbe72f3a
Siddarth Sharma <siddarthsharma1077@gmail.com>
2020-03-24 00:05:16 +0530

    Minimal Change

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ class Mailchimp extends React.Component {
             {...input}
             key={input.name}
             onChange={({ target }) => this.setState({ [input.name]: target.value })}
-            defaultValue={this.state[input.name]}
+            defaultValue={input.defaultValue}
           />
         )}
         <button
